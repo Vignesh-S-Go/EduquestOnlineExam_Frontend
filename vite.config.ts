3d9aae3620fd695d2214b3ac615c09cef4536186
Vignesh S <2300033852@kluniversity.in>
2025-09-15 17:43:18 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',   // 👈 ensures assets load relative to WAR context
+  base: '/frontapp1/',   // 👈 ensures assets load relative to WAR context
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
